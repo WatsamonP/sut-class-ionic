b5202a3d51b450172d11f5eaf8ee11da4e7fb77d
WatsamonP <pongsupan.watsamon@gmail.com>
2018-10-01 08:50:13 +0700

    HEROKU DEPLOYMENT.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+let express = require('express'),
+    path = require('path');
+var app = express();
+let server = require('http').Server(app);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
+
+app.get('/blog.html', function(req, res,next){
+    res.sendFile(path.join(__dirname+"/blog.html"));
+});
+
+app.post('/contact', function(req, res, next){
+
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log("App is running on port " + port);
+});
+
+//server.listen(process.env.PORT || port)
